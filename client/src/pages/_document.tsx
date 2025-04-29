3e734fca0f388aad17c33f02361384795a6c14a5
Uttam Kumar P <panasalauttam@gmail.com>
2025-04-29 11:35:39 -0500

    feat: Add saber SVG as favicon and update document head to include it

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/saber.svg" type="image/svg+xml" />
+      </Head>
       <body>
         <Main />
         <NextScript />
